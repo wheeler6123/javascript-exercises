0e73bb0e01c2e19c6ef842062d6661e4985a109a
John <wheeler6123@gmail.com>
2023-05-31 17:56:17 -0600

    add correct code to make reverseString function correctly

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str.split('').reverse().join('');
 };
+
+//ES6 syntax
+//const reverseString = str => str.split('').reverse().join('');
 
 // Do not edit below this line
 module.exports = reverseString;
