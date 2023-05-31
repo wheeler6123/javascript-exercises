31f54e13ee7fab8aabdee32429872fd08f4679fe
John <wheeler6123@gmail.com>
2023-05-31 17:52:55 -0600

    added code to make repeatString work

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let result = '';
+    if(num<0) return 'ERROR';
+    for(let i=0; i<num; i++){
+        result += str;
+    }
+    return result;
 };
+
+//ES6 syntax
+//const repeatString = (str,num) => num<0 ? 'ERROR' : str.repeat(num);
 
 // Do not edit below this line
 module.exports = repeatString;
