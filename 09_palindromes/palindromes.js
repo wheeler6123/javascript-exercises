7e93329a9951b8380e393783ab5c0e6cb5de1bcf
John <wheeler6123@gmail.com>
2023-06-01 16:52:32 -0600

    add solution for palindromes to test successfully

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let chars = str.toLowerCase().split('');
+    let letters = chars.filter(char => char.match(/[a-z]/));
+    return letters.join('') === letters.reverse().join('');
 };
 
 // Do not edit below this line
