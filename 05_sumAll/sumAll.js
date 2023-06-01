4716a2a91c3133dd20c97986ff40aa7de6819ec3
John <wheeler6123@gmail.com>
2023-05-31 18:11:25 -0600

    add correct code to make sumAll test successfully

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    if(!args.every(arg => typeof arg === 'number')) return 'ERROR';
+    const start = args.sort((a,b) => a-b)[0];
+    const end = args.sort((a,b) => a-b)[1];
+    if(start < 0 || end < 0) return 'ERROR';
+    let sum = 0;
+    for(let i=start; i<=end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
