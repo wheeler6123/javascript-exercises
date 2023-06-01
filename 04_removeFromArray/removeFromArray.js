b40b6e8b0402bae2c3626e74bc1f690538478af4
John <wheeler6123@gmail.com>
2023-05-31 18:05:05 -0600

    add correct code to make removeFromArray test successfully

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
 const removeFromArray = function() {
-
+    const array = arguments[0];
+    const itemsToRemove = Array.from(arguments).slice(1);
+    return array.filter(item => !itemsToRemove.includes(item));
 };
 
 // Do not edit below this line
