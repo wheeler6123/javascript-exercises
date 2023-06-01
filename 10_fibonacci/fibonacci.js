a3cae3ced7086b36310889c51bf6baa5c65d6ef6
John <wheeler6123@gmail.com>
2023-06-01 17:01:27 -0600

    add solution to make fibonacci test successfully

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(typeof num !== 'number') num = parseInt(num);
+    if(num < 0) return 'OOPS';
+    let fibArr = [];
+    for(let i=0; i<num; i++) {
+        if(i < 2) fibArr.push(1);
+        else fibArr.push(fibArr[i-1] + fibArr[i-2]);
+    }
+    return fibArr[num-1];
 };
 
 // Do not edit below this line
