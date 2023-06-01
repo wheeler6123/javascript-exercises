9fcf6360aee3d7c1a4b5009263750ffc3fd0f87b
John <wheeler6123@gmail.com>
2023-06-01 16:41:50 -0600

    add code solution to make calculator test successfully

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((a,c) => a+c, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((a,c) => a*c, 1);
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return Math.pow(x,y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if(x===0 || x===1) return 1;
+  return x * factorial(x-1);
 };
 
 // Do not edit below this line
